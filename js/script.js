befe2437ff98adfa55b5e06a4abc2beeb17aab3f
Patryk Szczerbinski <patryk.szczerbinski@o2.pl>
2022-07-03 19:21:28 +0200

    Added navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const navArrow = document.querySelector('.nav-arrow');
-const navUl = document.querySelector('.nav-ul');
+const navUl = document.querySelector('.nav-ul-wrapper');
 
 navArrow.addEventListener('click', () => {
     navArrow.classList.toggle('active');
